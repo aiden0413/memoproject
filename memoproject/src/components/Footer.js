9a233380540b93ce7a1e50742bec3d03a355c395
aiden0413 <aiden0413@naver.com>
2021-06-19 15:26:31 +0900

    header에 시간추가, 날씨정보 3초마다 갱신, footer

--- memoproject/src/components/Footer.js
+++ memoproject/src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="justify-start py-6 px-6 border border-gray-300 relative fixed">
-            김남오(english name, namo5), 노석준(english name,aiden0413), 최진우(Choi Jinwoo, Geenoos)
+        <div className="justify-start py-6 px-6 border border-gray-300 relative fixed text-xs text-center">
+            김남오(namo5), 노석준(aiden0413), 최진우(Geenoos)
         </div>
     )
 }
