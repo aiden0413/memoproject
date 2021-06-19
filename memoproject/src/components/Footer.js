117c992b7a785550d672de48f1126c552793c8f6
aiden0413 <aiden0413@naver.com>
2021-06-19 14:04:28 +0900

    주석삭제

--- memoproject/src/components/Footer.js
+++ memoproject/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div className="justify-start py-6 px-6 border border-gray-300 relative fixed">
-            Footer
+            김남오(english name, namo5), 노석준(english name,aiden0413), 최진우(Choi Jinwoo, Geenoos)
         </div>
     )
 }
